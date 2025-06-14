4d3b361e4304df46ac918d3069a3bf53b55a6cc3
Albert <albert@albertleal.com>
2025-06-14 13:21:16 +0200

    formating and readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Installation
+- Node 18
+- Playwright
+
+# Start
+```sh 
+npx playwright install 
+npx playwright install-deps
+npm install
+npm start
+```
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 import { chromium } from 'playwright';
 
 async function main() {
-  const browser = await chromium.launch({ headless: true });
-  const page = await browser.newPage();
-  try {
-    await page.goto('https://www.google.com');
-    const button = await page.$('input[name="btnK"]');
-    if (button) {
-      console.log('Search button found');
-    } else {
-      console.log('Search button not found');
+    const browser = await chromium.launch({ headless: true });
+    const page = await browser.newPage();
+    try {
+        await page.goto('https://www.google.com');
+        const button = await page.$('input[name="btnK"]');
+        if (button) {
+            console.log('Search button found');
+        } else {
+            console.log('Search button not found');
+        }
+    } catch (err) {
+        console.error('Error during scraping:', err);
+    } finally {
+        await browser.close();
     }
-  } catch (err) {
-    console.error('Error during scraping:', err);
-  } finally {
-    await browser.close();
-  }
 }
 
 main();
